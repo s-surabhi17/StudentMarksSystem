f4ea376d79a3c61fe999e93936e49a366106f3e6
Surabhi <deshpandesurabhi955@gmail.com>
2025-11-04 21:24:20 +0530

    Reorganized project: moved source files into main/test folders and added lib

--- main/StudentModule.java
+++ main/StudentModule.java
@@ -7,10 +7,23 @@ public class StudentModule {
         this.studentName = name;
     }
 
-    public int getStudentId() { return studentId; }
-    public String getStudentName() { return studentName; }
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
 
     public void displayStudent() {
         System.out.println("ID: " + studentId + ", Name: " + studentName);
     }
+
+    // ✅ Add this if missing
+    public static void main(String[] args) {
+        StudentModule s1 = new StudentModule(1, "Surabhi");
+        s1.displayStudent();
+    }
+}
+
 
